b6d5eceb26f98094e86e84dfa56601e3be42408f
granpWork <51110599+granpWork@users.noreply.github.com>
2024-05-01 16:13:59 +0800

    Add files via upload
    initial commit

---
+++ src/main/java/com/granp/render/torender/TorenderApplication.java
@@ -0,0 +1,13 @@
+package com.granp.render.torender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/granp/render/torender/TorenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.granp.render.torender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
